990949d120d85081c2f536c7aee6840d8738592f
JosephPenaQuino <joseph.pena@utec.edu.pe>
2019-04-07 19:12:16 -0500

    My first change

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MyVector
+by: Joseph Peña
--- Vector.cpp
+++ Vector.cpp
@@ -4,21 +4,33 @@
 
 #include "Vector.h"
 
-namespace UTEC {
-    void vector::push_back(const int &value) {
-        // Completarlo
+UTEC::vector::vector() : _arr{nullptr}, _size(0) {}
+
+UTEC::vector::~vector() {
+    // Completarlo
+}
+
+void UTEC::vector::push_back(const int &value) {
+    // Completarlo
+    if (_size == 0) {
+        _arr = new int[1];
     }
+    else if (_size < sizeof(*_arr)) {
 
-    void vector::pop_back() {
-        // Completarlo
     }
+    else {
 
-    vector::vector() : _arr{nullptr}, _size(0) {}
-    vector::~vector() {
-        // Completarlo
     }
+}
 
-    int vector::size() {return _size;}
+void UTEC::vector::pop_back() {
+    // Completarlo
+}
 
-    int vector::get_item(int i) {return _arr[i];}
+int UTEC::vector::size() {return _size;}
+
+int UTEC::vector::get_item(int i) {return _arr[i];}
+
+void UTEC::vector::insert(int pos, const int &value) {
+
 }
